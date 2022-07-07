b5e6301ea827439e306de74afec394013b5c26b9
livemehere <rhdxoals@gmail.com>
2022-07-08 02:55:11 +0900

    cors 허용

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { HttpExceptionFilter } from 'utils/exception.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
